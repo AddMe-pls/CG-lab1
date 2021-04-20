7c86dedfc01fe7a50abf014689944a00b46b6db8
Andrey Kuklin <andre.kuk@bk.ru>
2021-04-20 03:06:39 +0300

    Finished filters

--- QtConsoleApplication1/Filter.cpp
+++ QtConsoleApplication1/Filter.cpp
@@ -1,4 +1,8 @@
 #include "Filter.h"
+
+
+//___________________Functions_______________________________________________
+
 template <class T>
 T clamp(T value, T max, T min)
 {
@@ -9,6 +13,15 @@ T clamp(T value, T max, T min)
 	return value;
 }
 
+double RandDouble(int low, int high) {
+	double res;
+	return res = double(rand()) / RAND_MAX * (high - low) + low;
+}
+
+
+//______________________________Process__________________________________________
+
+//..........PointFilter____________________________________________________
 
 QImage Filter::process(const QImage& img) const
 {
@@ -20,8 +33,79 @@ QImage Filter::process(const QImage& img) const
 			QColor color = calcNewPixelColor(img, x, y);
 			result.setPixelColor(x, y, color);
 		}
+
 	return result;
 }
+
+QImage GreyWorldFilter::process(const QImage& img)
+{
+	for (int x = 0; x < img.width(); x++)
+		for (int y = 0; y < img.height(); y++)
+		{
+			QColor tmp = img.pixelColor(x, y);
+			Rs += tmp.red();
+			Gs += tmp.green();
+			Bs += tmp.blue();
+		}
+	
+	Rs /= img.width() * img.height();
+	Gs /= img.width() * img.height();
+	Bs /= img.width() * img.height();
+
+	AVG = (Rs + Gs + Bs) / 3;
+	
+	return Filter::process(img);
+}
+
+//........MatrixFilter______________________________________________________
+
+QImage Opening::process(const QImage& img) const
+{
+	QImage result;
+	int rad = mKernel.getRadius();
+	Dilation dil(rad);
+	result = dil.process(img);
+	Erosion eros(rad);
+	result = eros.process(result);
+	return result;
+}
+
+QImage Closing::process(const QImage& img) const
+{
+	QImage result;
+	int rad = mKernel.getRadius();
+	Erosion eros(rad);
+	result = eros.process(img);
+	Dilation dil(rad);
+	result = dil.process(result);
+	return result;
+}
+
+QImage Grad::process(const QImage& img) const
+{
+	QImage tmp1, tmp2, result(img);
+	int rad = mKernel.getRadius();
+	Dilation dil(rad);
+	tmp1 = dil.process(img);
+	Erosion eros(rad);
+	tmp2 = eros.process(img);
+
+	for (int x = 0; x < tmp1.width(); x++)
+		for (int y = 0; y < tmp2.height(); y++)
+		{
+			QColor color1 = tmp1.pixelColor(x, y);
+			QColor color2 = tmp2.pixelColor(x, y);
+			QColor res;
+			res.setRgb(clamp(color1.red() - color2.red(), 255, 0), clamp(color1.green() - color2.green(), 255, 0), clamp(color1.blue() - color2.blue(), 255, 0));
+			result.setPixelColor(x, y, res);
+		}
+
+	return result;
+}
+
+//____________________calcNewPixelColor___________________________________________
+
+//..........PointFilter_______________________________________________________
 
 QColor InvertFilter::calcNewPixelColor(const QImage& img, int x, int y) const
 {
@@ -29,6 +113,62 @@ QColor InvertFilter::calcNewPixelColor(const QImage& img, int x, int y) const
 	color.setRgb(255 - color.red(), 255 - color.green(), 255 - color.blue());
 	return color;
 }
+
+QColor GrayScaleFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	double Intensity = 0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue();
+	color.setRgb(Intensity, Intensity, Intensity);
+	return color;
+}
+
+QColor Sepia::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	double Intensity = 0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue();
+	color.setRgb(clamp(Intensity + 2 * k, 255.0, 0.0), clamp(Intensity + 0.5 * k, 255.0, 0.0), clamp(Intensity - k, 255.0, 0.0));
+	return color;
+}
+
+QColor BrightUp::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	color.setRgb(clamp(color.red() + C, 255, 0), clamp(color.green() + C, 255, 0), clamp(color.blue() + C, 255, 0));
+	return color;
+}
+
+QColor LinealStretching::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	color.setRgb(clamp(((color.red() - minR)*255/(maxR - minR)), 255.f, 0.f), clamp(((color.green() - minG) * 255 / (maxG - minG)), 255.f, 0.f), clamp(((color.blue() - minB) * 255 / (maxB - minB)), 255.f, 0.f));
+	return color;
+}
+
+QColor GreyWorldFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	color.setRgb(clamp((AVG * color.red() / Rs), 255.0, 0.0), clamp((AVG * color.green() / Gs), 255.0, 0.0), clamp((AVG * color.blue() / Bs), 255.0, 0.0));
+	return color;
+}
+
+QColor Transfer::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color;
+	if (x + x1 < img.width() && y + y1 < img.height())
+		color = img.pixelColor(x + x1, y + y1);
+	else
+		color.setRgb(0, 0, 0);
+	return color;
+}
+
+QColor Glass::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color;
+		color = img.pixelColor(clamp(x + (RandDouble(0, 1) - 0.5) * 10, double(img.width() - 1), 0.0), clamp(y + (RandDouble(0, 1) - 0.5) * 10, double(img.height() - 1), 0.0));
+	return color;
+}
+
+//..........MatrixFilter________________________________________________________________________
 
 QColor MatrixFilter::calcNewPixelColor(const QImage& img, int x, int y) const
 {
@@ -50,4 +190,101 @@ QColor MatrixFilter::calcNewPixelColor(const QImage& img, int x, int y) const
 		}
 
 	return QColor(clamp(returnR, 255.f, 0.f), clamp(returnG, 255.f, 0.f), clamp(returnB, 255.f, 0.f));
+}	
+
+QColor Dilation::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	float returnR = 0, tmpR = 0;
+	float returnG = 0, tmpG = 0;
+	float returnB = 0, tmpB = 0;
+	int size = mKernel.getSize();
+	int radius = mKernel.getRadius();
+	for (int i = -radius; i <= radius; i++)
+		for (int j = -radius; j <= radius; j++)
+		{
+			int idx = (i + radius) * size + j + radius;
+			QColor color = img.pixelColor(clamp(x + j, img.width() - 1, 0), clamp(y + i, img.height() - 1, 0));
+			tmpR = color.red() * mKernel[idx];
+			tmpG = color.green() * mKernel[idx];
+			tmpB = color.blue() * mKernel[idx];
+			if (tmpR > returnR)
+				returnR = tmpR;
+			if (tmpG > returnG)
+				returnG = tmpG;
+			if (tmpB > returnB)
+				returnB = tmpB;
+		}
+	return QColor(clamp(returnR, 255.f, 0.f), clamp(returnG, 255.f, 0.f), clamp(returnB, 255.f, 0.f));
+}
+
+QColor Erosion::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	float returnR = 255, tmpR = 0;
+	float returnG = 255, tmpG = 0;
+	float returnB = 255, tmpB = 0;
+	int size = mKernel.getSize();
+	int radius = mKernel.getRadius();
+	for (int i = -radius; i <= radius; i++)
+		for (int j = -radius; j <= radius; j++)
+		{
+			int idx = (i + radius) * size + j + radius;
+			QColor color = img.pixelColor(clamp(x + j, img.width() - 1, 0), clamp(y + i, img.height() - 1, 0));
+			tmpR = color.red() * mKernel[idx];
+			tmpG = color.green() * mKernel[idx];
+			tmpB = color.blue() * mKernel[idx];
+			if (tmpR < returnR)
+				returnR = tmpR;
+			if (tmpG < returnG)
+				returnG = tmpG;
+			if (tmpB < returnB)
+				returnB = tmpB;
+		}
+	return QColor(clamp(returnR, 255.f, 0.f), clamp(returnG, 255.f, 0.f), clamp(returnB, 255.f, 0.f));
+}
+
+QColor Opening::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor a;
+	return a;
+}
+
+QColor Closing::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor a;
+	return a;
+}
+
+QColor Grad::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor a;
+	return a;
+}
+
+QColor Median::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	int size = mKernel.getSize();
+	int radius = mKernel.getRadius();
+	int *masR, *masG, *masB;
+	masR = new int[size*size];
+	masG = new int[size*size];
+	masB = new int[size*size];
+
+	for (int i = -radius; i <= radius; i++)
+		for (int j = -radius; j <= radius; j++)
+		{
+			int idx = (i + radius) * size + j + radius;
+			QColor color = img.pixelColor(clamp(x + j, img.width() - 1, 0), clamp(y + i, img.height() - 1, 0));
+			masR[idx] = color.red();
+			masG[idx] = color.green();
+			masB[idx] = color.blue();
+		}
+
+	std::sort(masR, masR + size*size);
+	std::sort(masG, masG + size*size);
+	std::sort(masB, masB + size*size);
+
+	QColor col;
+	col.setRgb(clamp(masR[size*size / 2], 255, 0), clamp(masG[size*size / 2], 255, 0), clamp(masB[size*size / 2], 255, 0));
+
+	return col;
 }
--- QtConsoleApplication1/main.cpp
+++ QtConsoleApplication1/main.cpp
@@ -17,7 +17,85 @@ int main(int argc, char* argv[])
 
     img.load(QString(s.c_str()));
     img.save("Images/Source.png");
+    /*
+    char size[80];
+    std::ifstream ifs("KernelM.txt");
+    ifs.getline(size, 3, '\n');
+    int sizei = std::atoi(size);
+    Kernel MatKernel(sizei);
+    std::unique_ptr<float[]> tmp = std::make_unique<float[]>(sizei * sizei);
 
+    for (int i = 0; i < sizei * sizei; i++)
+    {
+        ifs.getline(size, 1, '\n');
+        tmp[i] = std::atoi(size);
+    }
+
+    MatKernel.SetKernel(tmp.get(), sizei / 2);
+    */
+    
     InvertFilter invert;
     invert.process(img).save("Images/Invert.png");
+
+    BlurFilter blur;
+    blur.process(img).save("Images/Blur.png");
+
+    GaussianFilter gauss;
+    gauss.process(img).save("Images/Gauss.png");
+
+    GrayScaleFilter gray;
+    gray.process(img).save("Images/Gray.png");
+
+    Sepia sep;
+    sep.process(img).save("Images/Sep.png");
+
+    BrightUp bright;
+    bright.process(img).save("Images/Bright.png");
+    
+    SharpnessUp sharpness;
+    sharpness.process(img).save("Images/SharpnessUp.png");
+    
+    GreyWorldFilter gworld;
+    gworld.process(img).save("Images/GreyWorld.png");
+    
+    Transfer trasf;
+    trasf.process(img).save("Images/Transfer.png");
+    
+    Glass glass;
+    glass.process(img).save("Images/Glass.png");
+    
+    Sharpness sharp;
+    sharp.process(img).save("Images/Sharpness.png");
+    
+    MotionBlur motblur;
+    motblur.process(img).save("Images/MotionBlur.png");
+    
+    Dilation dil;
+    dil.process(img).save("Images/Dilation.png");
+    
+    Erosion eros;
+    eros.process(img).save("Images/Erosion.png");
+    
+    Opening op;
+    op.process(img).save("Images/Opening.png");
+    
+    Closing cl;
+    cl.process(img).save("Images/Closing.png");
+    
+    LinealStretching lin(img);
+    lin.process(img).save("Images/LinealStretching.png");
+    
+    Grad grad;
+    grad.process(img).save("Images/Gradient.png");
+    
+    Median med;
+    med.process(img).save("Images/Median.png");
+    
+    SobelFilterX sobelx;
+    sobelx.process(img).save("Images/SobelX.png");
+
+    SobelFilterY sobelY;
+    sobelY.process(img).save("Images/SobelY.png");
+    
+    return 0;
 }
